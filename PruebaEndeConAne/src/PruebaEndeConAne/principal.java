ef4faacfa723d0861787b5278fd87a6d1d46b8de
susan <susan@DESKTOP-HDK9TU1>
2024-12-03 15:18:56 +0100

    commit Ane

--- PruebaEndeConAne/src/PruebaEndeConAne/principal.java
+++ PruebaEndeConAne/src/PruebaEndeConAne/principal.java
@@ -1,7 +1,13 @@
 package PruebaEndeConAne;
 
+import java.util.Scanner;
+
 public class principal {
 	public static void main(String[] args) {
+		String izena;
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Sartu zure izena");
+		izena = sc.nextLine();
 	}
 }
